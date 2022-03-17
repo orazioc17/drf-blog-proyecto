cad517abc9bd8b825733094544a6ae31f9c5d3f5
Orazio Cappadonna <oraziocappadonna1999@gmail.com>
2022-03-17 19:32:54 -0400

    Using JWT for authentication

--- proyectoblog/settings.py
+++ proyectoblog/settings.py
@@ -119,6 +119,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
